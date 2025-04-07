9c4730f3872af83d88ffabdc5c8c6ac642426e63
llhyeon <0617mosses@gmail.com>
2025-04-07 16:46:44 +0900

    [Feat] 로고 컴퍼넌트 생성

---
+++ app/components/common/logo.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+type LogoProps = {
+  width?: number;
+  height?: number;
+};
+
+export default function Logo({ width = 100, height = 100 }: LogoProps) {
+  return (
+    <>
+      <Image src={'/logo/logo.svg'} alt="로고" width={width} height={height} />
+    </>
+  );
+}
